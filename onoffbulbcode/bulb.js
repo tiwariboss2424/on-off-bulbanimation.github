ba5f6f3bfb2dacec630163401931b17d6c283415
Niranjan Tiwari <120399001+tiwariboss2424@users.noreply.github.com>
2022-12-24 00:09:28 +0530

    Add files via upload

---
+++ onoffbulbcode/bulb.js
@@ -0,0 +1,8 @@
+function glow(){
+    let image=document.querySelector(".image");
+    image.setAttribute("src","onbulb2.jpg");
+}
+function bulboff(){
+    let image=document.querySelector(".image")
+    image.setAttribute("src","offled1.jpg")
+}
